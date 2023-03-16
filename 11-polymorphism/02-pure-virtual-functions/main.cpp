c8bff9b237b28e8b1c7c1a17886532405d0a0533
Luis Jaramillo <dev.luisjn@gmail.com>
2023-03-15 23:58:40 -0500

    Polymorphism

---
+++ 11-polymorphism/02-pure-virtual-functions/main.cpp
@@ -0,0 +1,12 @@
+#include "main.hpp"
+
+int main()
+{
+  Cat c1("polly", 20.0, "cat");
+  Dog d1("spot", 30.0, "dog");
+
+  cout << "\nPolly Rate: $" << c1.calcDailyRate();
+  cout << "\nSpot Rate: $" << d1.calcDailyRate();
+
+  return 0;
+}
